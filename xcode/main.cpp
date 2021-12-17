03ffd08b8714d637bef348e99273792c4c88f101
Giulio <gformenti@rockefeller.edu>
2021-12-17 18:34:57 -0500

    Initial Commit

---
+++ xcode/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  xcode
+//
+//  Created by Giulio Formenti on 12/17/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
