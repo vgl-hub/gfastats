557f2d2d019340aad819f711c00431261fe8180b
gf777 <giulio.formenti@gmail.com>
2021-12-20 16:02:25 -0500

    int long long for genome size

--- src/fastats.cpp
+++ src/fastats.cpp
@@ -152,7 +152,8 @@ int main(int argc, char **argv) {
     
     if (stats_flag) {
         
-        int totScaffLen = 0, scaffN50 = 0, scaffNG50 = 0, totGapLen = 0, gapN = 0;
+        long long int64 totScaffLen = 0; 
+        int scaffN50 = 0, scaffNG50 = 0, totGapLen = 0, gapN = 0;
         
         std::vector<int> scaffLens;
         
