9946c4ccfe8c3ab6152911a3cfb7a902623125bf
angelobrajuka <brajuka.angelo@gmail.com>
2022-04-26 19:34:44 -0400

    added another sak validate file

--- src/gfastats-generate-tests.cpp
+++ src/gfastats-generate-tests.cpp
@@ -21,7 +21,7 @@ int main(int argc, char **argv) {
     };
 
     const std::map<std::set<std::string>, std::vector<std::string>> file_args = {
-        {{"random1.fasta"}, {"-k testFiles/random1.instructions.sak"}}
+        {{"random1.fasta"}, {"-k testFiles/random1.instructions.sak", "-ofa -k testFiles/random1.instructions.sak"}}
     //  {{set of test file paths}, {list of command line args to run with}}
     };
 
---
+++ validateFiles/random1.fasta19.tst
@@ -0,0 +1,6 @@
+testFiles/random1.fasta -ofa -k testFiles/random1.instructions.sak
+embedded
+>Scaffold2
+NNNTTCCTcgCACtCNNNNNGCACTAGCTCAANNN
+>Scaffold4
+TNNNNNNNNNNTGANNNCTN
