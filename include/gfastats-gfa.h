2bd75b402ec67860b3f17b90f9e366d063ace7bd
gf777 <giulio.formenti@gmail.com>
2022-05-27 23:28:42 -0400

    Update gfastats-gfa.h

--- include/gfastats-gfa.h
+++ include/gfastats-gfa.h
@@ -851,10 +851,10 @@ public:
             
         }
         
-        unsigned long long int seqLen = sequence->length()-1;
+        unsigned long long int seqLen = sequence->size()-1;
         for (char &base : *sequence) {
 
-            unsigned long long int count = 1;
+            unsigned int count = 1;
             if(hc_flag && hc_index < bedCoords.size() && pos == bedCoords[hc_index].first) {
                 count = bedCoords[hc_index].second - bedCoords[hc_index].first;
                 ++hc_index;
