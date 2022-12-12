f86c086e0daaaaf9e4f999504777b5d2d1ef3b03
gf777 <giulio.formenti@gmail.com>
2022-12-12 11:03:04 -0500

    multithreaded compatible custom getline function

--- src/input.cpp
+++ src/input.cpp
@@ -1,13 +1,12 @@
 #include <stdlib.h>
 #include <unistd.h>
-#include <string>
 #include <thread>
 #include <mutex>
 #include <vector>
 #include <queue>
 #include <stack>
 
-#include <iostream>
+#include <istream>
 #include <fstream>
 #include <sstream>
 
@@ -35,23 +34,59 @@
 #include "input-gfa.h"
 #include "input.h"
 
-//std::istream& getline(std::istream& stream, std::string& str)
-//{
-//  char ch;
-//  str.clear();
-//  while (stream.get(ch) && ch != '\n')
-//    str.push_back(ch);
-//  return stream;
-//}
-//
-//std::istream& getline(std::istream& stream, std::string& str, char sep)
-//{
-//  char ch;
-//  str.clear();
-//  while (stream.get(ch) && ch != sep)
-//    str.push_back(ch);
-//  return stream;
-//}
+std::istream& getline(std::istream& is, std::string& str) {
+    
+    str.clear();
+    std::ios_base::iostate err = std::ios_base::goodbit;
+    std::streamsize extr = 0;
+    
+    while (true)
+    {
+
+        int i = is.rdbuf()->sbumpc();
+        if (i == EOF)
+            break;
+        ++extr;
+        if (i == '\n')
+            break;
+        str.push_back(i);
+        
+    }
+    
+    if (extr == 0)
+        err |= std::ios_base::failbit;
+    is.setstate(err);
+    
+    return is;
+    
+}
+
+std::istream& getline(std::istream& is, std::string& str, char dlm) {
+    
+    str.clear();
+    std::ios_base::iostate err = std::ios_base::goodbit;
+    std::streamsize extr = 0;
+    
+    while (true)
+    {
+
+        int i = is.rdbuf()->sbumpc();
+        if (i == EOF)
+            break;
+        ++extr;
+        if (i == dlm)
+            break;
+        str.push_back(i);
+        
+    }
+    
+    if (extr == 0)
+        err |= std::ios_base::failbit;
+    is.setstate(err);
+    
+    return is;
+    
+}
 
 void Input::load(UserInput userInput) {
     
