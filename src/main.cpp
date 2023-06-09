d47d98492297b2f2ece17d1e4af6f74962301899
gf777 <giulio.formenti@gmail.com>
2023-06-09 12:59:33 -0400

    spelling typo..

--- src/main.cpp
+++ src/main.cpp
@@ -353,7 +353,7 @@ int main(int argc, char **argv) {
                     optind--;
                     
                     std::string file;
-                    uint8 i = 0;
+                    uint8_t i = 0;
 
                     for( ;optind < argc && !isInt(argv[optind]); optind++) {
                         
