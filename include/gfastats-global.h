bafac3847b7690ea8364f9f6ea3e8fb18e4a45bd
gf777 <giulio.formenti@gmail.com>
2023-12-15 15:00:26 -0500

    renaming global variable files for individual tools to avoid conflict with gfalibs global file

---
+++ include/gfastats-global.h
@@ -0,0 +1,15 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+#include <mutex>
+#include <chrono>
+#include <queue>
+#include <thread>
+#include <functional>
+
+#include "log.h"
+#include "threadpool.h"
+
+extern UserInput userInput;
+
+#endif /* GLOBAL_H */
